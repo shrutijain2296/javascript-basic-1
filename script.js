13d6edec053c6fbc0bcca0bc3c5e559b9451b1b0
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-13 22:06:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(value) { 
+    if(value.marks > 50){
+      console.log(value);  
+    }
+
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+
+  arr.forEach(function(value){
+    if (value.marks > 50) {
+      console.log(value);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id: 4, name: "susan", age: "20", marks: 45});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let filterArray = arr.filter((value) => value.marks < 50);
+  console.log(filterArray);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newStudents = [
+    {id: 4, name: "alice", age: "21", marks: 90},
+    {id: 5, name: "bob", age: "22", marks: 75},
+    {id: 6, name: "charlie", age: "20", marks: 60}
+  ];
+
+  let secondArray = arr.concat(newStudents);
+  console.log(secondArray);
+  console.log(arr);
 }
+
